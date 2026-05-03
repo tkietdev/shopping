d722e94fcabe24e94181d4f60bae03fa784927f8
Tuấn Kiệt( 22T1020183) <117705056+TUANKIETPHANHUU@users.noreply.github.com>
2026-05-03 12:26:38 +0700

    up

--- inc/database.php
+++ inc/database.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     class Database{
     static $con;
     public static function  getConnection(){
@@ -185,7 +186,7 @@ function navbar(){
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 style=margin: 0 auto">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="login.php">Login</a>
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -199,16 +200,37 @@ function navbar(){
     }
 
     $s .= '</ul>
-        </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
-        </li>
-            <li class="nav-item">
+        </li>';
+
+
+        if(!isset($_SESSION['user']))
+            $s .= '<li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+        </li>';
+        else{
+        $s .= '
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Hi ' . splitName($_SESSION['user']['name']) . '
+            </a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+            </ul>
+            </li>';
+        }
+
+
+
+            $s.='<li class="nav-item">
             <a class="nav-link" href="#">
             <img src="assets/icon/cart.png" width="25" height="25" alt="cart">
             <i>0</i>
             </a>
         </li>
+
+
+
         </ul>
         <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
@@ -299,5 +321,119 @@ function body(){
     }
 
     echo $s;
+}
+function login(){
+    if(isset($_POST['emailphone']) && isset($_POST['password'])){
+
+        $emailphone = $_POST['emailphone'];
+        $password = $_POST['password'];
+
+        $q = Database::query("
+            SELECT * FROM users
+            WHERE (email='$emailphone' OR phone='$emailphone')
+            AND password='$password'
+        ");
+
+        $r = $q->fetch_assoc();
+
+        if($r){
+            if($r['role'] == 'admin'){
+                header("location: admin.php");
+                exit();
+            }else{
+                $_SESSION['user'] = $r;
+                header("location: index.php");
+                exit();
+            }
+        }else{
+            $_SESSION['login_fail'] = 'Dữ liệu không chính xác!!';
+            header("location: login.php");
+            exit();
+        }
+    }
+    $s = '
+    <section class="vh-100">
+    <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid" alt="Sample image">
+        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+
+        <form action = "" method = "post">
+
+
+            <h2 style = "padding: 40px 0 25px 0">Login Information</h2>';
+
+        if(!empty($_SESSION['login_fail'])){
+    $s .= '<div style="color:red; margin-bottom:10px;">' . $_SESSION['login_fail'] . '</div>';
+    unset($_SESSION['login_fail']);
+}
+            $s.='<!-- Email input -->
+        <div data-mdb-input-init class="form-outline mb-4">
+            <input type="text" name="emailphone" class="form-control form-control-lg"
+    placeholder="Enter a valid email or phone number" />
+            </div>
+
+            <!-- Password input -->
+            <div data-mdb-input-init class="form-outline mb-3">
+            <input type="password" name="password" class="form-control form-control-lg"
+                placeholder="Enter password" />
+
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center">
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                <label class="form-check-label" for="form2Example3">
+                Remember me
+                </label>
+            </div>
+            <a href="#!" class="text-body">Forgot password?</a>
+            </div>
+
+            <div class="text-center text-lg-start mt-4 pt-2">
+            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don\'t have an account? <a href="#!"
+                class="link-danger">Register</a></p>
+            </div>
+
+        </form>
+        </div>
+    </div>
+    </div>
+    <div
+    <!-- Right -->
+    <div>
+        <a href="#!" class="text-white me-4">
+        <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+        <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+        <i class="fab fa-google"></i>
+        </a>
+        <a href="#!" class="text-white">
+        <i class="fab fa-linkedin-in"></i>
+        </a>
+    </div>
+    <!-- Right -->
+    </div>
+</section>
+    ';
+    echo $s;
+}
+function splitName($str) { // từ cuối cùng của chuỗi họ tên
+    $rs = NULL; // cái này cũng giúp kiểm tra chuỗi có ít nhất 2 từ hay không
+    //nếu chỉ có một từ, nó sẽ trả về kết quả NULL
+    $word = mb_split(' ', $str); // tách từ
+    $n = count($word)-1; // lấy vị trí mảng của từ cuối cùng
+    if ($n > 0) {$rs = $word[$n];} // lấy từ cuối cùng
+
+return $rs;
 }
 ?>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+include('inc/database.php');
+__header("Login Page");
+navbar();
+jumbotron();
+login();
+    __footer();
+?>
+
